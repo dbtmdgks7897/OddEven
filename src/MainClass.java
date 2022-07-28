1929419ab4e42ad5880984fd77d7eb231fda3b48
ysh <dbtmdgks7897@outlook.kr>
2022-07-28 16:38:38 +0900

    구슬 홀짝 게임

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MainClass {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int betMarble;
         int userMarble = 10;
         int ranMarble;
@@ -16,45 +16,56 @@ public class MainClass {
         System.out.println("10개의 구슬을 다 잃을 시 사망");
         System.out.println("게임 시작");
 
-        // 구슬 베팅
-        while(true){
-            System.out.print("배팅할 구슬의 갯수 : ");
-            betMarble = sc.nextInt(); // 숫자 스캔 후 입력 받기
-            if(betMarble > userMarble){
-                System.out.println("구슬 갯수 초과 ");
-            }else{
-                System.out.println("배팅된 구슬 갯수 : " + betMarble);
+
+        while (true){
+            // 구슬 베팅
+            while(true){
+                System.out.print("배팅할 구슬의 갯수 : ");
+                betMarble = sc.nextInt(); // 숫자 스캔 후 입력 받기
+                if(betMarble > userMarble){
+                    System.out.println("구슬 갯수 초과 ");
+                } else if (betMarble > (20 - userMarble)) {
+                    System.out.println("상대 구슬 갯수 모자람");
+                } else{
+                    System.out.println("배팅된 구슬 갯수 : " + betMarble);
+                    break;
+                }
+            }
+
+            // 랜덤 설정
+            Random random = new Random();
+            random.setSeed(System.currentTimeMillis());
+
+            // User 정답 입력
+            System.out.print("홀 / 짝 예측( 홀 / 짝 ) : ");
+            answer = sc.next();
+
+            // 난수로 홀 / 짝 생성
+            ranMarble = random.nextInt(10) + 1;
+            isOdd = ranMarble % 2 == 0 ? "짝" : "홀";
+            System.out.println("컴퓨터가 선택한 구슬 갯수 : " + ranMarble + isOdd);
+
+            // 정답 / 오답 판별 후 구슬 더하거나 빼기
+            if(answer.equals(isOdd)){
+                System.out.println("정답");
+                userMarble += betMarble;
+            }else {
+                System.out.println("오답");
+                userMarble -= betMarble;
+            }
+
+            System.out.println();
+            System.out.println("현재 구슬 갯수 : " + userMarble);
+            System.out.println("상대 구슬 갯수 : " + (20 - userMarble));
+
+            // 게임 종료
+            if(userMarble <= 0){
+                System.out.println("짐");
+                break;
+            } else if (userMarble >= 20) {
+                System.out.println("이김");
                 break;
             }
-        }
-
-        // 랜덤 설정
-        Random random = new Random();
-        random.setSeed(System.currentTimeMillis());
-
-        // User 정답 입력
-        System.out.print("홀 / 짝 예측( 홀 / 짝 ) : ");
-        answer = sc.next();
-
-        // 난수로 홀 / 짝 생성
-        ranMarble = random.nextInt(10) + 1;
-        isOdd = ranMarble % 2 == 0 ? "짝" : "홀";
-        System.out.println("컴퓨터가 선택한 구슬 갯수 : " + ranMarble + isOdd);
-
-        // 정답 / 오답 판별 후 구슬 더하거나 빼기
-        if(answer.equals(isOdd)){
-            System.out.println("정답");
-            userMarble += betMarble;
-        }else {
-            System.out.println("오답");
-            userMarble -= betMarble;
-        }
-
-        // 게임 종료
-        if(userMarble <= 0){
-            System.out.println("짐");
-        } else if (userMarble >= 20) {
-            System.out.println("이김");
         }
     }
 }
